b4460f087c272a16c1fb4d12b4588ba17b1c6028
Sebastian Dombrowsky <sebdombrowsky@web.de>
2022-01-21 16:46:19 +0100

    feature fertig, release build

---
+++ PaceRechner/App/pace_rechner/lib/pages/herzfrequenz_rechner_widgets/custom_slider.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class CustomSlider extends StatefulWidget {
+  TextEditingController prozent;
+  Function onChange;
+  CustomSlider({Key? key, required this.prozent, required this.onChange})
+      : super(key: key);
+
+  @override
+  State<CustomSlider> createState() => _CustomSliderState();
+}
+
+class _CustomSliderState extends State<CustomSlider> {
+  double currentValue = 50;
+  @override
+  Widget build(BuildContext context) {
+    return Slider(
+      value: currentValue,
+      max: 100,
+      divisions: 100,
+      label: currentValue.round().toString() + "%",
+      onChanged: (double value) {
+        setState(() {
+          currentValue = value;
+          widget.prozent.text = value.round().toString();
+          widget.onChange();
+        });
+      },
+    );
+  }
+}
--- PaceRechner/App/pace_rechner/lib/pages/herzfrequenz_rechner_widgets/herzfrequenz_page.dart
+++ PaceRechner/App/pace_rechner/lib/pages/herzfrequenz_rechner_widgets/herzfrequenz_page.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:pace_rechner/pages/herzfrequenz_rechner_widgets/custom_maxpulse_picker.dart';
+import 'package:pace_rechner/pages/herzfrequenz_rechner_widgets/custom_slider.dart';
 
 class HerzfrequenzPage extends StatefulWidget {
   final Size size;
@@ -13,10 +17,17 @@ class HerzfrequenzPage extends StatefulWidget {
 
 class _HerzfrequenzPageState extends State<HerzfrequenzPage> {
   TextEditingController pulsController = TextEditingController();
+  TextEditingController prozentController = TextEditingController();
+  TextEditingController zielPulsController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    pulsController.text = "190";
+    pulsController.text =
+        pulsController.text == "" ? "190" : pulsController.text;
+    zielPulsController.text =
+        zielPulsController.text == "" ? "95" : zielPulsController.text;
+    prozentController.text =
+        prozentController.text == "" ? "50" : prozentController.text;
     return Container(
       width: widget.size.width * 0.5,
       height: widget.size.height * 0.5,
@@ -29,48 +40,135 @@ class _HerzfrequenzPageState extends State<HerzfrequenzPage> {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Padding(
-              padding: const EdgeInsets.symmetric(vertical:12),
+              padding: const EdgeInsets.symmetric(vertical: 12),
               child: Text(
-                "Mit wie viel % deiner maximalen Herzfrequenz willst du trainieren?",
+                "Deine maximale Herzfrequenz",
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   color: Colors.black,
-                  fontSize: 22,
+                  fontSize: 18,
                   fontFamily: "Orbitron",
-                  fontWeight: FontWeight.bold
+                  fontWeight: FontWeight.bold,
                 ),
               ),
             ),
             Padding(
-              padding: const EdgeInsets.symmetric(vertical: 12),
+              padding: const EdgeInsets.symmetric(horizontal: 50),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        "Pulse",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 22,
+                            fontFamily: "Orbitron"),
+                      ),
+                      Text(
+                        "bpm",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 15,
+                            fontFamily: "Orbitron"),
+                      ),
+                    ],
+                  ),
+                  Material(
+                    borderOnForeground: true,
+                    elevation: 2,
+                    borderRadius: BorderRadius.circular(6),
+                    child: Container(
+                      width: 120,
+                      height: 60,
+                      decoration: BoxDecoration(
+                          color: Colors.grey[300],
+                          border: Border.all(color: Colors.grey[500]!),
+                          borderRadius: BorderRadius.circular(6)),
+                      child: Center(
+                        child: CustomMaxPulsePicker(
+                            controller: pulsController, onChange: () => null),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 12),
               child: Text(
-                "Trage hier deine maximale Herzfrequenz ein",
+                "Mit wie viel % deiner maximalen Herzfrequenz willst du trainieren?",
                 textAlign: TextAlign.center,
                 style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontFamily: "Orbitron",
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            CustomSlider(
+                prozent: prozentController,
+                onChange: () =>
+                    PulsBerechnen(pulsController.text, prozentController.text)),
+            Text(
+              "Für ein Training bei " +
+                  prozentController.text +
+                  "% deiner maximalen Herzfrequenz, liegt der optimale Pulsbereich bei",
+              textAlign: TextAlign.center,
+              style: TextStyle(
                   color: Colors.black,
-                  fontSize: 15,
+                  fontSize: 18,
                   fontFamily: "Orbitron",
+                  fontWeight: FontWeight.bold),
+            ),
+            Container(
+              width: widget.size.width,
+              decoration: BoxDecoration(
+                color: Colors.white,
+              ),
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12),
+                  child: FittedBox(
+                    fit: BoxFit.fitWidth,
+                    child: Text(
+                      zielPulsController.text,
+                      style: TextStyle(
+                        color: CupertinoColors.destructiveRed,
+                        fontSize: 75,
+                        fontFamily: "Orbitron",
+                      ),
+                    ),
+                  ),
                 ),
               ),
             ),
-            Material(
-              borderOnForeground: true,
-              elevation: 2,
-              borderRadius: BorderRadius.circular(6),
-              child: Container(
-                width: 120,
-                height: 60,
-                decoration: BoxDecoration(
-                    color: Colors.grey[300],
-                    border: Border.all(color: Colors.grey[500]!),
-                    borderRadius: BorderRadius.circular(6)),
-                child: Center(
-                    child: CustomMaxPulsePicker(controller: pulsController, onChange: () => null),),
+            Text(
+              "bpm",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 22,
+                fontFamily: "Orbitron",
               ),
-            ),
+            )
           ],
         ),
       ),
     );
+  }
+
+  PulsBerechnen(String maxPuls, String prozent) {
+    //maxPuls und prozent kann nicht null oder unconvertable sein
+    double _maxPuls = double.parse(maxPuls);
+    double _prozent = double.parse(prozent);
+
+    int zielPuls = (_maxPuls * (_prozent / 100)).round();
+    setState(() {
+      print(zielPuls.toString() + zielPulsController.text);
+      zielPulsController.text = zielPuls.toString();
+    });
   }
 }
